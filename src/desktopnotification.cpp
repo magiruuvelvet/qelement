56b47240406e521d360df8adde784159645e1b21
magiruuvelvet <magiruuvelvet@naver.com>
2020-07-31 12:52:30 +0200

    app: implemented most of libnotify support

--- src/desktopnotification.cpp
+++ src/desktopnotification.cpp
@@ -1,24 +1,30 @@
 #include "desktopnotification.hpp"
 
 #include <QApplication>
+#include <QColorSpace>
 
 // conflicts with glib
 #undef signals
 
+// note: don't use QString for libnotify functions, but std::string::c_str()
+//       libnotify does weird things and Valgrind shows errors in QString destructor
+
 #ifdef LIBNOTIFY_ENABLED
 #include <libnotify/notify.h>
 #include <libnotify/notification.h>
+#include <gdk-pixbuf/gdk-pixbuf.h>
 #endif
 
 #ifdef LIBNOTIFY_ENABLED
+static std::string d_appname;
 static void d_libnotify_init(const QString &appname)
 {
     static bool is_init = false;
     if (!is_init)
     {
-        const auto utf8name = appname.toUtf8().constData();
-        notify_init(utf8name);
-        notify_set_app_name(utf8name);
+        d_appname = appname.toStdString();
+        notify_init(d_appname.c_str());
+        notify_set_app_name(d_appname.c_str());
         is_init = true;
     }
 }
@@ -28,42 +34,122 @@ DesktopNotification::DesktopNotification()
 {
 #ifdef LIBNOTIFY_ENABLED
     d_libnotify_init(qApp->applicationDisplayName());
-    this->d_ptr = notify_notification_new("", "", "");
+    this->d_ptr = notify_notification_new("", "", nullptr);
 #endif
 }
 
-DesktopNotification::DesktopNotification(const QString &title, const QString &message, const QImage &image)
-    : _title(title),
-      _message(message),
-      _image(image)
+DesktopNotification::DesktopNotification(const QString &title, const QString &message, const QImage &image, const std::function<void()> &click_callback)
+    : DesktopNotification()
 {
-#ifdef LIBNOTIFY_ENABLED
-    d_libnotify_init(qApp->applicationDisplayName());
-    this->d_ptr = notify_notification_new(_title.toUtf8().constData(), _message.toUtf8().constEnd(), "");
-#endif
+    this->_title = title;
+    this->_message = message;
+    this->_image = image;
+    this->clickcb = click_callback;
+
+    this->update_notification_properties();
 }
 
 DesktopNotification::~DesktopNotification()
 {
 #ifdef LIBNOTIFY_ENABLED
-    // note: calling free() causes double free or corruption error
-    this->d_ptr = nullptr;
+    // crashes with invalid pointer
+    //std::free((NotifyNotification*)this->d_ptr);
 #endif
+
+    if (this->d_img)
+    {
+        std::free(this->d_img);
+        this->d_img = nullptr;
+    }
+
+    this->d_ptr = nullptr;
 }
 
-void DesktopNotification::send(const QString &title, const QString &message, const QImage &image)
+void DesktopNotification::setTitle(const QString &title)
 {
-#ifdef LIBNOTIFY_ENABLED
-    d_libnotify_init(qApp->applicationDisplayName());
+    this->_title = title;
+    this->update_notification_properties();
+}
 
-    auto notification = notify_notification_new(title.toUtf8().constData(), message.toUtf8().constData(), "");
-    notify_notification_show(notification, nullptr);
-#endif
+void DesktopNotification::setMessage(const QString &message)
+{
+    this->_message = message;
+    this->update_notification_properties();
+}
+
+void DesktopNotification::setImage(const QImage &image)
+{
+    this->_image = image;
+    this->update_notification_properties();
+}
+
+void DesktopNotification::setClickCallback(const std::function<void()> &click_callback)
+{
+    this->clickcb = click_callback;
+    this->update_notification_properties();
+}
+
+void DesktopNotification::send(const QString &title, const QString &message, const QImage &image, const std::function<void()> &click_callback)
+{
+    DesktopNotification notification(title, message, image, click_callback);
+    notification.send();
 }
 
 void DesktopNotification::send() const
 {
 #ifdef LIBNOTIFY_ENABLED
-    notify_notification_show((NotifyNotification*)d_ptr, nullptr);
+    notify_notification_show((NotifyNotification*)this->d_ptr, nullptr);
+#endif
+}
+
+void DesktopNotification::update_notification_properties()
+{
+#ifdef LIBNOTIFY_ENABLED
+    const std::string title_c = _title.toStdString();
+    const std::string message_c = _message.toStdString();
+    const void *image_c = this->convert_image(_image);
+
+    notify_notification_update((NotifyNotification*)this->d_ptr, title_c.c_str(), message_c.c_str(), nullptr);
+    notify_notification_set_image_from_pixbuf((NotifyNotification*)this->d_ptr, (GdkPixbuf*)image_c);
+
+//    auto action = [](NotifyNotification *n, gchar *action, gpointer data) {
+//        typedef void (*callback_ptr)();
+//        ((callback_ptr)data)();
+//    };
+
+//    notify_notification_clear_actions((NotifyNotification*)this->d_ptr);
+//    notify_notification_add_action((NotifyNotification*)this->d_ptr, "DoAction", "default", action, &this->clickcb, nullptr);
+#endif
+}
+
+const void *DesktopNotification::convert_image(const QImage &image)
+{
+    if (image.isNull())
+    {
+        return nullptr;
+    }
+
+#ifdef LIBNOTIFY_ENABLED
+
+    // remove old image is present
+    if (this->d_img)
+    {
+        std::free(this->d_img);
+        this->d_img = nullptr;
+    }
+
+    QImage backup(image);
+    backup.convertToColorSpace(QColorSpace::SRgb);
+    backup = backup.convertToFormat(QImage::Format_RGBA8888);
+
+    // copy image data
+    unsigned char *b = static_cast<unsigned char*>(std::malloc(backup.sizeInBytes()));
+    memcpy(b, backup.constBits(), backup.sizeInBytes());
+    this->d_img = b;
+
+    int stride = backup.width() * 4;
+    return gdk_pixbuf_new_from_data(b, GDK_COLORSPACE_RGB, true, 8, backup.width(), backup.height(), stride, nullptr, nullptr);
+#else
+    return nullptr;
 #endif
 }
--- src/desktopnotification.hpp
+++ src/desktopnotification.hpp
@@ -3,18 +3,22 @@
 #include <QString>
 #include <QImage>
 
+#include <functional>
+
 // TODO: experimental libnotify implementation, possible memory leaks and crashes
 class DesktopNotification
 {
 public:
     DesktopNotification();
-    DesktopNotification(const QString &title, const QString &message, const QImage &image = {});
+    DesktopNotification(const QString &title, const QString &message, const QImage &image = {}, const std::function<void()> &click_callback = []{});
     ~DesktopNotification();
 
-    // works
-    static void send(const QString &title, const QString &message, const QImage &image = {});
+    void setTitle(const QString &title);
+    void setMessage(const QString &message);
+    void setImage(const QImage &image);
+    void setClickCallback(const std::function<void()> &click_callback);
 
-    // doesn't work for some reason :/
+    static void send(const QString &title, const QString &message, const QImage &image = {}, const std::function<void()> &click_callback = []{});
     void send() const;
 
 private:
@@ -22,5 +26,11 @@ private:
     QString _message;
     QImage _image;
 
+private:
+    void update_notification_properties();
+    const void *convert_image(const QImage &image);
     void *d_ptr = nullptr;
+    unsigned char *d_img = nullptr;
+
+    std::function<void()> clickcb = []{};
 };
